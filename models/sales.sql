b2206c4548ed5ffd1d4f5f0fe8227b8b6ea23bbf
Lari <larissagd@gmail.com>
2023-08-09 13:55:45 +0000

    fixxxxx

--- models/sales.sql
+++ models/sales.sql
@@ -1,15 +1,6 @@
 {{ config(schema="transaction") }}
 
-/*{% macro margin(s, p) %}
-    {{ s }}.revenue - ({{ s }}.quantity * cast({{ p }}.purchse_price as float64))
-{% endmacro %}
 
-{% macro margin_percent(turnover, purchase_cost, precision=2) %}
-    round(
-        safe_divide(({{ turnover }} - {{ purchase_cost }}), {{ turnover }}),
-        {{ precision }}
-    )
-{% endmacro %} */
 with
     sales as (select * from {{ ref('stg_sales') }}),
     product as (select * from {{ ref('stg_product') }})
